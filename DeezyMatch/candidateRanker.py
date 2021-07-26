e8fbcedb8c65d60df29ddb3ae86bc2897e109a52
Fernanda Alvares Freire <83588057+fernandaalvaf@users.noreply.github.com>
2021-07-26 15:25:40 +0200

    lines 328-331: from 0.99 to 1.01

--- DeezyMatch/candidateRanker.py
+++ DeezyMatch/candidateRanker.py
@@ -326,8 +326,8 @@ def candidate_ranker(input_file_path="default", query_scenario=None, candidate_s
                 if query_candidate_pd["faiss_dist"].max() > (selection_threshold*1.01):
                     break
             elif ranking_metric.lower() in ["cosine"]:
-                # 0.99 is multiplied to avoid issues with float numbers and rounding errors
-                if query_candidate_pd["cosine_dist"].max() > (selection_threshold*0.99):
+                # 1.01 is multiplied to avoid issues with float numbers and rounding errors
+                if query_candidate_pd["cosine_dist"].max() > (selection_threshold*1.01):
                     break 
     
             # Go to the next zone    
